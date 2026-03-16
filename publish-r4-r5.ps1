484f9289961718e583c3042dd780ee539f2dd9c2
Giorgio Cangioli <giorgio.cangioli@gmail.com>
2026-03-16 09:12:49 +0100

    FHIR-55518, FHIR-55510 : Clarified use of characteristic and Strength Modelling in Medication

--- publish-r4-r5.ps1
+++ publish-r4-r5.ps1
@@ -33,7 +33,7 @@ $originalLocation = Get-Location
 # Default commit message if none provided
 if ([string]::IsNullOrWhiteSpace($Message)) {
   $Message = @"
-FHIR-55553: typos fixing
+FHIR-55518, FHIR-55510 : Clarified use of characteristic and Strength Modelling in Medication
 "@
 }
 
