af78c3461c2332f58e94cb823930a41da75893f3
Giorgio Cangioli <giorgio.cangioli@gmail.com>
2026-04-17 11:35:24 +0200

    Updated observation-value-r5 alias to correct the URL and modified dependency structure in sushi-config.yaml (for sushi 3.19)

--- publish-r4-r5.ps1
+++ publish-r4-r5.ps1
@@ -33,7 +33,7 @@ $originalLocation = Get-Location
 # Default commit message if none provided
 if ([string]::IsNullOrWhiteSpace($Message)) {
   $Message = @"
-restored configuration for previous sushi version
+Updated observation-value-r5 alias to correct the URL and modified dependency structure in sushi-config.yaml (for sushi 3.19)
 "@
 }
 
